2390a2ba9da46dbc7e3b39421d814368fd39be8d
Logan Tellier <logan.tellier@outlook.com>
2021-03-13 17:05:20 -0500

    chore: update pricepal_main to match conventions
    Non-functional changes to the pricepal_main file to adhere to naming, import, and whitespace conventions.

--- pricepal/pricepal_main.py
+++ pricepal/pricepal_main.py
@@ -10,7 +10,6 @@ from PyQt5 import uic, QtWidgets
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-import matplotlib.pyplot as plt
 
 # Local Application Libraries
 from gui.ui_utils import find_form
@@ -19,37 +18,35 @@ from common.status_logger import QTextEditLogger
 
 # =====================================================================
 
-print('passed')
-
 class PricePalMainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(PricePalMainWindow, self).__init__()
 
         # Load the main window GUI file
-        uic.loadUi(find_form('pricepal_mainform.ui'),self)
+        uic.loadUi(find_form('pricepal_mainform.ui'), self)
 
         # Add the custom matplotlib widget to the defined groupbox
         self.plotwindow = MplWidget(self.groupBox_Graph)
 
         # Add the status logging textbox to the statusbar
         logging.getLogger().setLevel(logging.DEBUG)
-        self.logStatusBox = QTextEditLogger(self.statusbar)
-        self.logStatusBox.setFormatter(
+        self.log_status_box = QTextEditLogger(self.statusbar)
+        self.log_status_box.setFormatter(
             logging.Formatter(
                 '%(asctime)s | %(levelname)s | %(module)s | %(message)s'
             )
         )
-        logging.getLogger().addHandler(self.logStatusBox)
+        logging.getLogger().addHandler(self.log_status_box)
 
         # Add a rolling log file for storing more verbose logs
         # Will create a new file when near to the specified maximum, keeping up to backupCount number of copies
-        self.logFile = RotatingFileHandler('data/logs/pricepal-log.log', maxBytes=100000, backupCount=9)
-        self.logFile.setFormatter(
+        self.rotating_log_file = RotatingFileHandler('data/logs/pricepal-log.log', maxBytes=100000, backupCount=9)
+        self.rotating_log_file.setFormatter(
             logging.Formatter(
                 '%(asctime)s | %(levelname)s | Module: %(module)s | Function: %(funcName)s | %(message)s'
             )
         )
-        logging.getLogger().addHandler(self.logFile)
+        logging.getLogger().addHandler(self.rotating_log_file)
 
         logging.info('')
         logging.info('Start to application session.')
@@ -67,9 +64,8 @@ class MplWidget(QtWidgets.QWidget):
         self.canvas = FigureCanvas(self.figure)
 
         vertical_layout = QtWidgets.QVBoxLayout()
-        vertical_layout.alignment
         vertical_layout.addWidget(self.canvas)
-        vertical_layout.addWidget(NavigationToolbar(self.canvas,self))
+        vertical_layout.addWidget(NavigationToolbar(self.canvas, self))
 
         self.canvas.axes = self.canvas.figure.add_subplot(111)
         self.figure.tight_layout()
@@ -79,9 +75,8 @@ class MplWidget(QtWidgets.QWidget):
 
 def main():
     autograph_app = QtWidgets.QApplication(sys.argv)
-    mainWindow = PricePalMainWindow()
+    main_window = PricePalMainWindow()
     autograph_app.exec_()
-
 
 if __name__ == '__main__':
     main()
